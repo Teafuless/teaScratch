e175e7d12ad0a2b15d4bee8c029d9915782a6bc5
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-11-25 22:30:07 +0500

    Update teanded.js

--- teanded.js
+++ teanded.js
@@ -1,56 +1,77 @@
 class teandedScratch {
-    getInfo() {
-      return {
-        id: 'teanded',
-        name: 'Teanded Scratch',
-        blocks: [
-          {
-            opcode: 'turnInToTea',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Превратить в чай [TEXT]',
-            arguments: {
-              TEXT: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'Teaful'
-              }
-            }
-          },
-          {
-            opcode: 'getPosDirection',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Получить направление позиции X: [X], Y: [Y] с симуляцией позиции X: [XX], Y: [YY]',
-            arguments: {
-              X: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              Y: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '0'
-              },
-              XX: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '50'
-              },
-              YY: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '50'
-              }
+  getInfo() {
+    return {
+      id: 'teanded',
+      name: 'Teanded Scratch',
+      blocks: [
+   
+        {
+          opcode: 'turnInToTea',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Превратить в чай [TEXT]',
+          arguments: {
+            TEXT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Teaful'
             }
           }
-        ]
-      };
+        },
+        {
+          opcode: 'getPosDirection',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Получить направление позиции X: [X], Y: [Y] с симуляцией позиции X: [XX], Y: [YY]',
+          arguments: {
+            X: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '0'
+            },
+            Y: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '0'
+            },
+            XX: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '50'
+            },
+            YY: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: '50'
+            }
+          }
+        },
+        {
+          opcode: 'returnValue',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'вернуть значение [TEAS]',
+          arguments: {
+              TEAS: {
+                  type: ArgumentType.STRING,
+                  menu: 'teaMenu'
+              }
+          }
+        }
+
+      ],
+      menus: {
+        teaMenu: {
+            items: ['teaful','tealess','teafuless']
+        }
     }
-    turnInToTea({TEXT}) {
-      return TEXT+" - чай"
-    }
-    getPosDirection({X,Y,XX,YY}){
-        let xr = XX - X
-        let yr = YY - Y
-        let res = (!Y>YY) ? 1 : 0
-        let ress = res * 180
-        let res2 = Math.atan(xr/yr)
-        return res2 + ress
-    }
+    };
   }
+  turnInToTea({TEXT}) {
+    return TEXT+" - чай"
+  }
+  getPosDirection({X,Y,XX,YY}){
+      let xr = XX - X
+      let yr = YY - Y
+      let res = (!Y>YY) ? 1 : 0
+      let ress = res * 180
+      let res2 = Math.atan(xr/yr)
+      return res2 + ress
+  }
+  returnValue({TEAS}) {
+return TEAS
+  }
+}
 Scratch.extensions.register(new teandedScratch());
