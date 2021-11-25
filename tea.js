8d300b10591a3a99183886a8ec2b45a1dc0ebd65
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-11-25 21:14:50 +0500

    Update tea.js

--- tea.js
+++ tea.js
@@ -1,8 +1,8 @@
-class turnTea {
+class teandedScratch {
     getInfo() {
       return {
-        id: 'turnintotea',
-        name: 'Turn In To Tea',
+        id: 'teanded',
+        name: 'Teanded Scratch',
         blocks: [
           {
             opcode: 'turnInToTea',
@@ -14,12 +14,41 @@ class turnTea {
                 defaultValue: 'Teaful'
               }
             }
+          },
+          {
+            opcode: 'getPosDirection',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Получить направление позиции X: [X], Y: [Y] с симуляцией позиции X: [XX], Y: [YY]',
+            arguments: {
+              X: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '0'
+              },
+              Y: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '0'
+              },
+              XX: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '50'
+              },
+              YY: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '50'
+              }
+            }
           }
         ]
       };
     }
     turnInToTea({TEXT}) {
       return TEXT+" - чай"
+    }
+    getPosDirection({X,Y,XX,YY}){
+        let xr = X - XX
+        let yr = Y - YY
+        let res = (yr < 0) ? 1 : 0
+        return Math.atan(xr/yr) + res * 180
     }
   }
 Scratch.extensions.register(new turnTea());
