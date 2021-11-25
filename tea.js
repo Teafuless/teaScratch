f6419bf55b1fa4e665cd204acbc7e4e67670ea6c
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-11-25 19:55:42 +0500

    Add files via upload

---
+++ tea.js
@@ -0,0 +1,25 @@
+class turnTea {
+    getInfo() {
+      return {
+        id: 'turnintotea',
+        name: 'Turn In To Tea',
+        blocks: [
+          {
+            opcode: 'strictlyEquals',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Превратить в чай [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Teaful'
+              }
+            }
+          }
+        ]
+      };
+    }
+    turnTea(args) {
+      return args.TEXT+" - чай"
+    }
+  }
+  Scratch.extensions.register(new turnTea());
