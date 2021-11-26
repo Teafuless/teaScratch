c4c386e7b55148d8f121c4e5628a9a6348f018c8
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-11-26 14:00:42 +0500

    Update teandedScrat.js

--- teandedScrat.js
+++ teandedScrat.js
@@ -3,184 +3,19 @@ class teandedScratch {
   getInfo() {
     return {
       
-      id: 'teanded',
-      color1: '#8DC286',
+      id: 'teadev',
+      color1: '#FBDC32',
 menuIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
  blockIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
-      name: 'Teanded Scratch',
+      name: 'Teadev ',
       blocks: [
    
         {
-          opcode: 'turnInToTea',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Превратить в чай [TEXT]',
+          opcode: 'hideSprite',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Скрыть спрайт',
           arguments: {
-            TEXT: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teaful'
-            }
-          }
-        },
-         {
-          opcode: 'randomText',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Выдать случайное слово [WORD] или [WORD2]',
-          arguments: {
-            WORD: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teaful'
-            },
-                        WORD2: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teafuless'
-            }
-          }
-        },
-         {
-          opcode: 'replaceIn',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Заменить [WORD] в [TEXT] на [NEW]',
-          arguments: {
-            TEXT: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teaful'
-            },
-            WORD: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'ful'
-            },
-            NEW: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: '0'
-            }
-          }
-        },
-        {
-          opcode: 'replaceAll',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Заменить все [WORD] в [TEXT] на [NEW]',
-          arguments: {
-            TEXT: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teaful'
-            },
-            WORD: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'ful'
-            },
-            NEW: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: '0'
-            }
-          }
-        },
-        {
-          opcode: 'replaceTimes',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Заменить [WORD] в [TEXT] на [NEW], [HM] раз',
-          arguments: {
-            TEXT: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Teaful'
-            },
-            WORD: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'ful'
-            },
-            NEW: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: '0'
-            },
-            HM: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '1'
-            }
-          }
-        },
-        {
-          opcode: 'getPosDirection',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Получить направление позиции X: [X], Y: [Y] с симуляцией позиции X: [XX], Y: [YY]',
-          arguments: {
-            X: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '0'
-            },
-            Y: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '0'
-            },
-            XX: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '50'
-            },
-            YY: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '50'
-            }
-          }
-        },
-        {
-          opcode: 'returnValue',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'вернуть значение [TEAS]',
-          arguments: {
-              TEAS: {
-                  type: Scratch.ArgumentType.STRING,
-                  menu: 'teaMenu'
-              }
-          }
-        },
-        {
-          opcode: 'shortIf',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Если [CONDITION], то [THEN], иначе [ELSE]',
-          arguments: {
-              CONDITION: {
-                  type: Scratch.ArgumentType.BOOLEAN
-              },
-              THEN: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'then'
-            },
-            ELSE: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'else'
-          }
-          }
-        },
-        {
-          opcode: 'ostatok',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Остаток от деления [NUM1] на [NUM2]',
-          arguments: {
-              NUM1: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: '5'
-              },
-              NUM2: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '2'
-            }
-          }
-        },
-        {
-          opcode: 'stepen',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'Возвести [NUM1] в степень [NUM2]',
-          arguments: {
-              NUM1: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: '2'
-              },
-              NUM2: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: '2'
-            }
-          }
         }
-        
-
       ],
       menus: {
         teaMenu: {
@@ -189,53 +24,8 @@ menuIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
     }
     };
   }
-  turnInToTea({TEXT}) {
-    return TEXT+" - чай"
+  hideSprite {
+    this.visible = false;
   }
-  getPosDirection({X,Y,XX,YY}){
-      let xr = XX - X
-      let yr = YY - Y
-      let res = (!Y>YY) ? 1 : 0
-      let ress = res * 180
-      let res2 = Math.atan(xr/yr)
-      return res2 + ress
-  }
-  replaceIn({TEXT,WORD,NEW}) {
-   return TEXT.replace(WORD,NEW)
-  }
-  replaceAll({TEXT,WORD,NEW}) {
-    let res;
-var res1 = new RegExp(WORD, 'g');
-res = TEXT.replace(res1, NEW);
-    return res 
-  }
-  replaceTimes({TEXT,WORD,NEW,HM}) {
-    let res = TEXT.replace(WORD,NEW)
-    for (let i = 0;i!=HM-1;i++) {
-   res = res.replace(WORD,NEW)
-    }
-    return res
-  }
-  randomText({WORD,WORD2}) {
-    function random(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-    
-    return (random(1,2)==1) ? WORD : WORD2
-  }
-  returnValue({TEAS}) {
-return TEAS
-  }
-shortIf({CONDITION,THEN,ELSE}){
-  return (CONDITION) ? THEN : ELSE
-}
-ostatok({NUM1,NUM2}){
-  return NUM1 % NUM2
-}
-stepen({NUM1,NUM2}){
-  return NUM1 ** NUM2
-}
 }
 Scratch.extensions.register(new teandedScratch());
