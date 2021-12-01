c71d3fa22e2916fac79f4dcb480fe0492174f11c
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-12-01 22:22:54 +0500

    Update teandedScrat.js

--- teandedScrat.js
+++ teandedScrat.js
@@ -791,13 +791,8 @@ stepen({NUM1,NUM2}){
    return eval(EVAL)
   }
   onSomething({CONDITION}) {
-    this.update()
-    if (CONDITION===true) {
-      return true
-  } else { 
-    return false 
-  }
-    return false
+if ((CONDITION)==true) { true }
+    false
   }
 newVar({NAME}){
 globalThis[NAME] = 0
