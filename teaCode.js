f28a218dc8db3c68d1c98bdc013d18e6fa76fc2c
Teafuless <95038883+Teafuless@users.noreply.github.com>
2021-12-19 23:47:17 +0500

    Create teaCode.js

---
+++ teaCode.js
@@ -0,0 +1,17 @@
+Code = {
+'get': 'return',
+'+': 'sum',
+'and': '\,',
+'[': '(',
+']': ')',
+'{': '[\'',
+'}': '\']',
+'send': 'console',
+'in': '.',
+'console': 'log',
+'variable': 'globalThis',
+'create': 'globalThis',
+'setValue': '=',
+'also': ' '
+}
+exports.Code = Code
