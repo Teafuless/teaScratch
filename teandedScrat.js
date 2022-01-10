badda2590adc46ad37bcf16e52f1f53eceaa3c72
Teafuless <95038883+Teafuless@users.noreply.github.com>
2022-01-10 13:47:26 +0500

    Update teandedScrat.js

--- teandedScrat.js
+++ teandedScrat.js
@@ -4,7 +4,7 @@ class teandedScratch {
     return {
       
       id: 'teadev',
-      color1: '#FBDC32',
+      color1: '#CDBBD2',
 menuIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
  blockIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
       name: 'Teadev ',
@@ -14,6 +14,13 @@ menuIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
           opcode: 'hideSprite',
           blockType: Scratch.BlockType.COMMAND,
           text: 'Скрыть спрайт',
+          arguments: {
+        }
+        },
+        {
+          opcode: 'getRotationStyleOfSprite',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'вид направления спрайта',
           arguments: {
         }
         }
@@ -27,6 +34,9 @@ menuIconURI: 'https://ghcdn.rawgit.org/Teafuless/teaScratch/main/favicon.png',
   }
   hideSprite() {
  Blockly.Blocks['looks_hide']
+  }
+  getRotationStyleOfSprite(_, util) {     
+    return util.target.rotationStyle || ''; 
   }
 }
 Scratch.extensions.register(new teandedScratch());
